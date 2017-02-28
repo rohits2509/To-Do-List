68aebb08470a830282f76fea841ab74354a9f396
rohits2509 <rohits2509@gmail.com>
2017-02-27 23:38:19 -0800

    todo list deletion and event bubbling

--- assests/js/todos.js
+++ assests/js/todos.js
@@ -1,21 +1,13 @@
 
 $("li").on("click", function(){
-	//if gray
-	if($(this).css("color") === "rgb(128, 128, 128)"){
-		//turn black
-		$(this).css({
-		color:"black",
-		textDecoration:"none"
+	$(this).toggleClass("completed");
+});
+
+$("span").click(function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
 	});
-	}	
-	//else
-	else{
-		//turn gray
-		$(this).css({
-		color:"gray",
-		textDecoration:"line-through"
-	});
-	}
-	
-	
+	event.stopPropagation();
+
+
 });
